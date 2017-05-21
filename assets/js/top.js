80387d70d55ebabdea0daa63c70d1d0750a0ce92
eeenbnb <eee.kz.www@gmail.com>
2017-05-21 21:01:16 +0900

    2nd karaage page

---
+++ assets/js/top.js
@@ -0,0 +1,25 @@
+//append tori
+$(function(){
+  var tori =emojione.toImage(':chicken:');
+  tori = $.parseHTML(tori);
+  $(tori).addClass('animated');
+  $(tori).attr('id','tori');
+  $("#emoji").append(tori);
+});
+//click tori
+$(function(){
+  $('#tori').click(function(){
+    $("#tori").removeClass('rubberBand');
+    $("#tori").addClass('bounceOutUp');
+    console.log('click tori');
+
+    $('.box-karaage').fadeOut(1000);
+    $('.box-tori').fadeIn(1000);
+  });
+});
+//load event
+$(window).on("load", function(){
+  $("#main").show();
+  $(".spinner").fadeOut("1000");
+  $(".animated").addClass('rubberBand');
+});
