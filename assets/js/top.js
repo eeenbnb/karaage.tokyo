6218f876f176e5d3222fc5eb28ec313b54776f17
eeenbnb <eee.kz.www@gmail.com>
2017-05-22 10:59:38 +0900

    change picture

--- assets/js/top.js
+++ assets/js/top.js
@@ -8,14 +8,13 @@ $(function(){
 });
 //click tori
 $(function(){
-  $('#tori').click(function(){
-    $("#tori").removeClass('rubberBand');
-    $("#tori").addClass('bounceOutUp');
-    console.log('click tori');
-
-    $('.box-karaage').fadeOut(1000);
-    $('.box-tori').fadeIn(1000);
-  });
+  // $('#tori').click(function(){
+  //   $("#tori").removeClass('rubberBand');
+  //   $("#tori").addClass('bounceOutUp');
+  //   console.log('click tori');
+  //   $('.box-tori').fadeIn(1000);
+  //   $('.box-karaage').fadeOut(1000);
+  // });
 });
 //load event
 $(window).on("load", function(){
